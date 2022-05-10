8999ff24f7141e913dcaeb6bd9be5c0d0f7bf108
Michael B <michaelbarr24@hotmail.com>
2022-05-10 16:28:49 +0100

    Add scroll to top

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Nosotros from "./components/Nosotros/Nosotros.js";
 import Footer from "./components/Footer/Footer.js";
 import Buscar from "./components/Buscar/Buscar.js";
 import Instrument from "./components/Buscar/Instrument.js";
+import ScrollToTop from "./components/default/ScrollToTop.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/mispartituras" element={<Home />} />
-        <Route path="/nosotros" element={<Nosotros />} />
-        <Route path="/buscar" element={<Buscar />} />
-        <Route path="/buscar/:instrument" element={<Instrument />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/mispartituras" element={<Home />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/buscar" element={<Buscar />} />
+          <Route path="/buscar/:instrument" element={<Instrument />} />
+        </Routes>
+      </ScrollToTop>
       <Footer />
     </div>
   );
